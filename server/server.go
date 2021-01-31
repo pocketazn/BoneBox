e5f57afad828866a8703433ae60b6724e4275acc
vvoshipt <43911520+vvoshipt@users.noreply.github.com>
2021-01-31 01:04:09 -0800

    connect server to 8080 and hook up docker

--- server/server.go
+++ server/server.go
@@ -19,12 +19,12 @@ func New(r *mux.Router) Server {
 }
 
 func (s *Server) Setup() {
-	s.srv.Addr = "127.0.0.1:8000"
+	s.srv.Addr = "127.0.0.1:8080"
 	s.srv.WriteTimeout = 15 * time.Second
 	s.srv.ReadTimeout = 15 * time.Second
 }
 
 
 func (s *Server) Run() {
-	s.Run()
+	s.srv.ListenAndServe()
 }
