9725863ff3a3312b30613ee9f74db08abca4ee31
vvoshipt <43911520+vvoshipt@users.noreply.github.com>
2021-02-01 18:22:56 -0800

    yml fix and init migrations

---
+++ migrations/20210102_155921_create_bones_schema.down.sql
@@ -0,0 +1,1 @@
+drop table bonebox.bones cascade;
---
+++ migrations/20210102_155921_create_bones_schema.up.sql
@@ -0,0 +1,11 @@
+create schema if not exists bonebox;
+
+create table if not exists bonebox.bones(
+    id bigint not null generated always as identity primary key,
+    name text not null, -- short name
+    description text, -- lengthy description
+    externam_label text, -- if an external source has their own identifier they want to associate with the bone for auditing
+    created_at timestamptz not null default now(),
+    updated_at timestamptz not null default now(),
+    archived_at timestamptz
+    );
